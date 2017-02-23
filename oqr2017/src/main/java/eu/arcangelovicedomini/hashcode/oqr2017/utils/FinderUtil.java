efa5e32d5cee472fca2aa9f8137cb1a796dfbe3f
ITGuy9401 <io@arcangelovicedomini.eu>
2017-02-23 21:11:11 +0100

    dev

--- oqr2017/src/main/java/eu/arcangelovicedomini/hashcode/oqr2017/utils/FinderUtil.java
+++ oqr2017/src/main/java/eu/arcangelovicedomini/hashcode/oqr2017/utils/FinderUtil.java
@@ -4,18 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 import eu.arcangelovicedomini.hashcode.oqr2017.FileReader.Data;
+import eu.arcangelovicedomini.hashcode.oqr2017.data.CacheServer;
 import eu.arcangelovicedomini.hashcode.oqr2017.data.Endpoint;
+import eu.arcangelovicedomini.hashcode.oqr2017.data.Request;
 import eu.arcangelovicedomini.hashcode.oqr2017.data.Video;
 
 public class FinderUtil {
 	
 	private static Map<Endpoint, Map<Video, Long>> videoRequests = new HashMap<>();
+	private static Map<Endpoint, Map<CacheServer, Long>> cacheServerLatency = new HashMap<>();
 	
 	public static void initializer(Data data) {
-		
+		for (Endpoint endpoint : data.endpoints) {
+			if (videoRequests.get(endpoint) == null) {
+				videoRequests.put(endpoint, new HashMap<>());
+			}
+			for (Request r : endpoint.getRequests()) {
+				videoRequests.get(endpoint).put(r.getVideo(), r.getRequests());
+			}
+		}
 	}
 	
 	public static Long findRequestForVideo(Endpoint e, Video v) {
-		return videoRequests.get(e).get(v).longValue();
+		return videoRequests.get(e).get(v);
+	}
+	
+	public static Long getLatency(Endpoint e, CacheServer s) {
+		return cacheServerLatency.get(e).get(s);
 	}
 }
