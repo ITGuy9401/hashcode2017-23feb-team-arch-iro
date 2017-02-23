a00f694da45ccc3cd1573c63d2af70a30b7fcdb5
ITGuy9401 <io@arcangelovicedomini.eu>
2017-02-23 21:33:48 +0100

    dev

--- oqr2017/src/main/java/eu/arcangelovicedomini/hashcode/oqr2017/App.java
+++ oqr2017/src/main/java/eu/arcangelovicedomini/hashcode/oqr2017/App.java
@@ -1,6 +1,7 @@
 package eu.arcangelovicedomini.hashcode.oqr2017;
 
 import eu.arcangelovicedomini.hashcode.oqr2017.FileReader.Data;
+import eu.arcangelovicedomini.hashcode.oqr2017.FileWriter.Output;
 
 /**
  * Hashcode 2017 - Team arch-iro
@@ -13,12 +14,25 @@ import eu.arcangelovicedomini.hashcode.oqr2017.FileReader.Data;
 public class App {
 
 	public static void main(String[] args) {
-		if (args == null || args.length != 1) {
-			throw new IllegalArgumentException("Wrong input. Pass as first (and only) argument "
-					+ "the relative or absolute path of the file encoded in UTF-8");
+		if (args == null || args.length != 2) {
+			throw new IllegalArgumentException("Wrong input. Pass as first argument "
+					+ "the relative or absolute path of the file encoded in UTF-8 and "
+					+ "as second argument the relative or absolute path of the output file");
 		}
 		
-		FileReader reader = new FileReader(args[0]);
+		String inputFile = args[0];
+		String outputFile = args[1];
+
+		FileReader reader = new FileReader(inputFile);
 		Data data = reader.processFile();
+		
+		Output output = new Output();
+		//FIXME valorizzare output
+		FileWriter writer = new FileWriter();
+		try {
+			writer.writeFile(output, outputFile);
+		} catch (Throwable th) {
+			throw new RuntimeException("Error while writing file: " + th.getMessage(), th);
+		}
 	}
 }
--- oqr2017/src/main/java/eu/arcangelovicedomini/hashcode/oqr2017/FileWriter.java
+++ oqr2017/src/main/java/eu/arcangelovicedomini/hashcode/oqr2017/FileWriter.java
@@ -1,5 +1,38 @@
 package eu.arcangelovicedomini.hashcode.oqr2017;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
+import eu.arcangelovicedomini.hashcode.oqr2017.FileWriter.Output.ServerInfo;
+
 public class FileWriter {
 
+	public static class Output {
+		public static class ServerInfo {
+			public Long serverId;
+			public Long[] videoIds;
+		}
+
+		public ServerInfo[] infos;
+	}
+	
+	public void writeFile(Output output, String filePath) throws Exception {
+		int numberOfServerUsed = output.infos.length;
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(numberOfServerUsed).append("\n");
+		
+		for (int i = 0; i < numberOfServerUsed; i++) {
+			ServerInfo serverInfo = output.infos[i];
+			sb.append(serverInfo.serverId);
+			for(Long id : serverInfo.videoIds) {
+				sb.append(" ").append(id);
+			}
+			sb.append("\n");
+		}
+		
+		FileOutputStream fos = new FileOutputStream(filePath);
+		fos.write(sb.toString().getBytes());
+		fos.close();
+	}
 }
